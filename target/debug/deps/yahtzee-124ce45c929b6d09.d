de63dfbdc4c9f59c6fa65034d772bd92b7b6f2aa
Gur-Sin <gurnoorjagjotsingh@gmail.com>
2025-11-07 02:59:23 -0500

    Removed rust files

--- target/debug/deps/yahtzee-124ce45c929b6d09.d
+++
@@ -1,5 +0,0 @@
-/home/gursin/programming/projects/yahtzee/target/debug/deps/yahtzee-124ce45c929b6d09.d: src/main.rs
-
-/home/gursin/programming/projects/yahtzee/target/debug/deps/yahtzee-124ce45c929b6d09: src/main.rs
-
-src/main.rs:
--- target/debug/yahtzee.d
+++
@@ -1,1 +0,0 @@
-/home/gursin/programming/projects/yahtzee/target/debug/yahtzee: /home/gursin/programming/projects/yahtzee/src/main.rs
